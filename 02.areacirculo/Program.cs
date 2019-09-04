8ad5924d4349dd9c83cd56176d27092d40735018
Carlos Arteaga <ciat117@gmail.com>
2019-09-04 11:06:29 -0500

    listo

---
+++ 02.areacirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _02.areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio=0;
+            double area=0;
+            Console.Clear();
+            Console.WriteLine("Programa que calcula el area de un circulo");
+            Console.WriteLine("Dame el radio ");
+            radio = float.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(radio,2);
+            Console.Write($" El area es {area}");
+        }
+    }
+}
