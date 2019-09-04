928840f5dcc505900ecb53cd9fd75ecbb78fae12
Carlos Arteaga <ciat117@gmail.com>
2019-09-04 18:12:45 -0500

    listo

---
+++ 12.vectorinverso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _12.vectorinverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[15];
+            Random rnd = new Random();
+            for(int i=0; i<A.Length; i++)  {
+                A[i]= rnd.Next(0,15);
+            }
+            Console.WriteLine("\nEl vector A"); 
+            imprime(A);
+            // hacer inverso el vector
+            int[] B = new int[15];
+            int cont=0;
+            for (int i = A.Length-1; i >= 0; i--)
+            {
+                B[cont]=A[i];
+                cont++;
+            }
+            Console.WriteLine();
+            imprime(B); 
+        
+        }
+        static void imprime(int[] v) 
+        {
+                for(int i=0; i<v.Length; i++)
+                    Console.Write($"{v[i]} ");
+        }
+        
+    }
+}
